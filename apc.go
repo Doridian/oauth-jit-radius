e83200250dfbf962f321934c82c4ddd531c1c737
Doridian <git@doridian.net>
2024-09-05 12:43:40 -0700

    dont do numbers

--- apc.go
+++ apc.go
@@ -14,11 +14,11 @@ func APCMapper(packet *radius.Packet, info OAuthUserInfo) (bool, error) {
 
 	var apcServiceType vendor_radius.APCServiceType
 	switch strings.ToLower(info.APCServiceType) {
-	case "admin", "1":
+	case "admin":
 		apcServiceType = vendor_radius.APCServiceType_Value_Admin
-	case "device", "2":
+	case "device":
 		apcServiceType = vendor_radius.APCServiceType_Value_Device
-	case "readonly", "3":
+	case "readonly":
 		apcServiceType = vendor_radius.APCServiceType_Value_ReadOnly
 	default:
 		return false, nil
